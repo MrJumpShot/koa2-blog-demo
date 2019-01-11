a66bb7b472d7f3ebcd0b7df27e7f8be2d0143c9e
alex_pro <1550921408@qq.com>
2019-01-11 10:04:12 +0800

    index change

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Koa = require("koa")
-
+const Router = require("koa-router")
 const app = new Koa()
+const router = new Router()
+router.get("/", async(ctx, next)=>{
+	ctx.body = "index page"
+})
 
 
+app.use(router.routes()).use(router.allowedMethods())
 app.listen(3000, function(){
 	console.log("the server is running on port 3000....")
 })
